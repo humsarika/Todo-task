7ed337e86a85491a2feacc14fd281381931d9330
Sarika Kushwaha <sarika.vsa01@gmail.com>
2024-05-24 02:21:56 +0530

    made changes in api call

--- src/App.js
+++ src/App.js
@@ -1,60 +1,68 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'; // Import Axios for making HTTP requests
-import TodoItem from './components/TodoItem';
-import DeleteConfirmationPopup from './components/DeleteConfirmationPopup';
-import AddTodoNotificationPopup from './components/AddTodoNotificationPopup';
-import './style.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios"; // Import Axios for making HTTP requests
+import TodoItem from "./components/TodoItem";
+import DeleteConfirmationPopup from "./components/DeleteConfirmationPopup";
+import AddTodoNotificationPopup from "./components/AddTodoNotificationPopup";
+import "./style.css";
 
 const App = () => {
   const [todos, setTodos] = useState([]); // Array to store todos
-  const [newTodo, setNewTodo] = useState(''); // String to store new todo item
+  const [newTodo, setNewTodo] = useState(""); // String to store new todo item
   const [visibleTodos, setVisibleTodos] = useState(10); // Tracking the number of visible todos
   const [showPopup, setShowPopup] = useState(false); // State to manage popup visibility
-  const [showDeletePopup, setShowDeletePopup] = useState(false);
-  const [todoToDelete, setTodoToDelete] = useState(null);
-  const [showAddNotificationPopup, setShowAddNotificationPopup] = useState(false);
+  const [showDeletePopup, setShowDeletePopup] = useState(false); // State variable to manage the visibility of the delete confirmation popup
+  const [todoToDelete, setTodoToDelete] = useState(null); // State variable to store the ID of the todo item to be deleted
+  const [showAddNotificationPopup, setShowAddNotificationPopup] =
+    useState(false); // State variable to manage the visibility of the add todo notification popup
 
-// Fetch todos from the API when the component mounts
-useEffect(() => {
-  // Making a GET request to fetch todos from the JSONPlaceholder API
-    axios.get('https://jsonplaceholder.typicode.com/todos')
-      .then(response => setTodos(response.data))
-      .catch(error => console.error('Error fetching todos:', error));
+  // Fetch todos from the API when the component mounts
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/todos"
+        );
+        setTodos(response.data);
+      } catch (error) {
+        console.error("Error fetching todos:", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   // Function to add a new todo
   const addTodo = () => {
-    if (newTodo.trim() === '' || newTodo.trim().length === 0) 
-      {setShowPopup(true); // Show popup if newTodo is empty
-    return;}
+    if (newTodo.trim() === "" || newTodo.trim().length === 0) {
+      setShowPopup(true); // Show popup if newTodo is empty
+      return;
+    }
 
     const newTodoItem = {
       userId: 1,
       id: todos.length + 1,
       title: newTodo,
-      completed: false
+      completed: false,
     };
     setTodos([newTodoItem, ...todos]);
-    setNewTodo('');
+    setNewTodo("");
   };
 
-
   // -----
-  const deleteTodo = id => {
+  const deleteTodo = (id) => {
     setTodoToDelete(id);
     setShowDeletePopup(true);
   };
 
-  const confirmDelete = id => {
+  const confirmDelete = (id) => {
     // Delete todo logic
     setShowDeletePopup(false);
-    setTodos(todos.filter(todo => todo.id !== todoToDelete));
+    setTodos(todos.filter((todo) => todo.id !== todoToDelete));
   };
 
   const cancelDelete = () => {
     setShowDeletePopup(false);
   };
-
 
   const showMoreTodos = () => {
     // incremeting number of todos on every click of show more button to show 10 more todos
@@ -68,21 +76,29 @@ useEffect(() => {
         <input
           type="text"
           value={newTodo}
-          onChange={e => setNewTodo(e.target.value)}
+          onChange={(e) => setNewTodo(e.target.value)}
           placeholder="Add a new todo"
         />
-        <button className='add-button' onClick={addTodo}>Add</button>
+        <button className="add-button" onClick={addTodo}>
+          Add
+        </button>
       </div>
       <div className="todo-container">
         <div className="todo-list">
-          {todos.slice(0, visibleTodos).map(todo => (
-            <TodoItem key={todo.id} todo={todo} deleteTodo={() => deleteTodo(todo.id)} />
+          {todos.slice(0, visibleTodos).map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              deleteTodo={() => deleteTodo(todo.id)}
+            />
           ))}
         </div>
       </div>
       {/* visible Todos */}
       {visibleTodos < todos.length && (
-        <button className="show-more-button" onClick={showMoreTodos}>Show More</button>
+        <button className="show-more-button" onClick={showMoreTodos}>
+          Show More
+        </button>
       )}
 
       {/* Popup to let user now todo is empty*/}
@@ -95,8 +111,8 @@ useEffect(() => {
         </div>
       )}
 
-       {/* Delete Confirmation Popup */}
-       {showDeletePopup && (
+      {/* Delete Confirmation Popup */}
+      {showDeletePopup && (
         <DeleteConfirmationPopup
           onCancel={cancelDelete}
           onConfirm={confirmDelete}
@@ -105,12 +121,12 @@ useEffect(() => {
 
       {/* Add Todo Notification Popup */}
       {showAddNotificationPopup && (
-        <AddTodoNotificationPopup onClose={() => setShowAddNotificationPopup(false)} />
+        <AddTodoNotificationPopup
+          onClose={() => setShowAddNotificationPopup(false)}
+        />
       )}
-
     </div>
   );
 };
 
 export default App;
-
